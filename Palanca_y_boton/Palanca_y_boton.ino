b8c0e92be8cadb8174bae50f660413b0ad253781
Facundo Jaime <164009725+HolyTostadora@users.noreply.github.com>
2024-08-06 08:46:45 -0300

    .

--- Palanca_y_boton/Palanca_y_boton.ino
+++ Palanca_y_boton/Palanca_y_boton.ino
@@ -1,9 +1,14 @@
 #include <Keyboard.h>
 
-#define ABAJO1 3
-#define ARRIBA1 2
+#define ABAJO1 2
+#define ARRIBA1 3
 #define DERECHA1 5
 #define IZQUIERDA1 4
+
+#define ABAJO2 9
+#define ARRIBA2 8
+#define DERECHA2 11
+#define IZQUIERDA2 10
 
 #define BTN_R 7
 
@@ -17,6 +22,11 @@ void setup()
   pinMode(ARRIBA1,INPUT_PULLUP);
   pinMode(DERECHA1,INPUT_PULLUP);
   pinMode(IZQUIERDA1,INPUT_PULLUP);  
+  pinMode(ABAJO2,INPUT_PULLUP);
+  pinMode(ARRIBA2,INPUT_PULLUP);
+  pinMode(DERECHA2,INPUT_PULLUP);
+  pinMode(IZQUIERDA2,INPUT_PULLUP);  
+  
   pinMode(BTN_R,INPUT_PULLUP);
 
   Keyboard.begin();
@@ -30,6 +40,11 @@ void loop()
   BTN_on_click(ABAJO1,'s');
   BTN_on_click(DERECHA1,'d');
   BTN_on_click(IZQUIERDA1,'a');
+
+  BTN_on_click(ARRIBA2,KEY_UP_ARROW);
+  BTN_on_click(ABAJO2,KEY_DOWN_ARROW);
+  BTN_on_click(DERECHA2,KEY_RIGHT_ARROW);
+  BTN_on_click(IZQUIERDA2,KEY_LEFT_ARROW);
 
   BTN_on_click(BTN_R,'r');
 
