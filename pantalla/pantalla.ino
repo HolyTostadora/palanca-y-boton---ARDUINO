777b0a7f31fac4e7ca65b79a94ce7c9392b92430
Facundo Jaime <164009725+HolyTostadora@users.noreply.github.com>
2024-08-12 15:58:19 -0300

    no funciona la pantalla

--- pantalla/pantalla.ino
+++ pantalla/pantalla.ino
@@ -1,5 +1,24 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+
+#define CS 2
+#define RESET 3
+#define DC 4
+#define MOSI 5
+#define SCK 6
+#define LED 7
+#define MISO 8
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341 (CS,DC,MOSI,SCK,RESET,MISO);
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,HIGH);
+
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(ILI9341_BLUE);
 
 }
 
