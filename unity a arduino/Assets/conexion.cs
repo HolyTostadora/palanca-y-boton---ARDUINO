77bcf9f84d4aeeb457c01689974e85be9a4c9a7f
HolyTostadora <164009725+HolyTostadora@users.noreply.github.com>
2024-08-27 08:28:12 -0300

    conexion unity arduino---unity

---
+++ unity a arduino/Assets/conexion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class conexion : MonoBehaviour
+{
+    SerialPort arduinoPort = new SerialPort("COM 4");
+
+    private void Awake()
+    {
+        arduinoPort.BaudRate = 9600;
+        arduinoPort.Parity = Parity.None;
+        arduinoPort.StopBits = StopBits.One;
+        arduinoPort.DataBits = 8;
+        arduinoPort.Handshake = Handshake.None;
+    }
+
+    void Start()
+    {
+
+        arduinoPort.Open();
+
+    }
+
+    public void SendMessagetoArduino (string msg)
+    {
+        arduinoPort.WriteLine(msg);
+    }
+
+    public void ClosePort()
+    {
+        arduinoPort.Close();  
+    }
+}
