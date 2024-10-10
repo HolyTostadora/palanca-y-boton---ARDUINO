287ed0cc84420ff51f69ec2b164efa76ce6c90af
HolyTostadora <164009725+HolyTostadora@users.noreply.github.com>
2024-10-10 14:27:26 -0300

    Create TiraLed.ino

---
+++ TiraLed/TiraLed.ino
@@ -0,0 +1,19 @@
+#include <FastLED.h>
+
+#define NUM_LEDS 5
+#define LED_PIN 3
+
+CRGB leds [NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812B,LED_PIN,GRB>(leds,NUM_LEDS);
+  FastLED.setBrightness(50);
+
+}
+
+void loop() {
+  leds[0] = CRGB:: Red;
+  leds[1] = CRGB:: Green;
+  leds[2] = CRGB:: Blue;
+
+}
