2c723cea1042d6821c0726e1c2224a4464f50dd1
HolyTostadora <164009725+HolyTostadora@users.noreply.github.com>
2024-10-31 11:00:41 -0300

    selector de personaje, pntos de tennis y frogger hecho
    falta el frogger individual y lo del street fighter

--- pantaDespues/pantaDespues.ino
+++ pantaDespues/pantaDespues.ino
@@ -1,3 +1,9 @@
+const int maxStrings = 10; // Máximo número de elementos en el array
+struct Result {
+  int id;
+  String msgArray[maxStrings];
+};
+
 #define LCD_CS A3 
 #define LCD_CD A2 
 #define LCD_WR A1 
@@ -20,14 +26,7 @@ MCUFRIEND_kbv tft;
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-
-const int maxStrings = 10; // Máximo número de elementos en el array
-
-
-struct Result {
-  int id;
-  String msgArray[maxStrings];
-};
+Result prueba;
 
 Result string2array(String string2convert) {
   Result res;
@@ -59,69 +58,90 @@ Result string2array(String string2convert) {
   return res;
 }
 
-void mostrarSeleccionPersonajes(String msgArray[]) {
-  tft.fillScreen(WHITE);
+void mostrarSeleccion2Personajes(Result mensaje) {
+  tft.fillScreen(BLACK);
 
   // Configuración para Player 1 en la mitad superior de la pantalla
-  tft.setCursor(40, 60);  // Posicionar en la mitad superior de la pantalla
-  tft.setTextSize(3);
+  tft.setCursor(130, 60);  // Posicionar en la mitad superior de la pantalla
+  tft.setTextSize(5);
   tft.print("Player 1");
-  
-  tft.setCursor(40, 100);  // Un poco más abajo para el nombre del personaje
-  tft.setTextSize(2);
-  tft.print(msgArray[0]);  // Mostrar el personaje que selecciona Player 1
+
+  tft.setCursor(180, 100);  // Un poco más abajo para el nombre del personaje
+  tft.setTextSize(4);
+  tft.print(mensaje.msgArray[0]);  // Mostrar el personaje que selecciona Player 1
 
   // Configuración para Player 2 en la mitad inferior de la pantalla
-  tft.setCursor(40, 180);  // Posicionar en la mitad inferior de la pantalla
-  tft.setTextSize(3);
+  tft.setCursor(130, 200);  // Posicionar en la mitad inferior de la pantalla
+  tft.setTextSize(5);
   tft.print("Player 2");
-  
-  tft.setCursor(40, 220);  // Un poco más abajo para el nombre del personaje
-  tft.setTextSize(2);
-  tft.print(msgArray[1]);  // Mostrar el personaje que selecciona Player 2
+
+  tft.setCursor(180, 240);  // Un poco más abajo para el nombre del personaje
+  tft.setTextSize(4);
+  tft.print(mensaje.msgArray[1]);  // Mostrar el personaje que selecciona Player 2
 }
 
-void mostrarPuntuaciones(String msgArray[]) {
-  tft.fillScreen(WHITE);
+void mostrarSeleccion1Personajes(Result mensaje) {
+  tft.fillScreen(BLACK);
+
+  // Configuración para Player 1 en la mitad superior de la pantalla
+  tft.setCursor(90, 85);  // Posicionar en la mitad superior de la pantalla
+  tft.setTextSize(7);
+  tft.print("Player 1");
+
+  tft.setCursor(140, 200);  // Un poco más abajo para el nombre del personaje
+  tft.setTextSize(6);
+  tft.print(mensaje.msgArray[0]);  // Mostrar el personaje que selecciona Player 1
+}
+
+void mostrarPuntuacionesTennis(Result mensaje) {
+  tft.fillScreen(BLACK);
 
   // Configuración para Player 1
-  tft.setTextSize(3);  // Tamaño de texto más grande para Player 1
-  tft.setCursor(40, 40);  // Posicionar en la parte superior
+  tft.setTextSize(5);  // Tamaño de texto más grande para Player 1
+  tft.setCursor(125, 40);  // Posicionar en la parte superior
   tft.print("Player 1");
-  
-  tft.setTextSize(2);  // Tamaño de texto más pequeño para los sets
-  tft.setCursor(40, 80);  // Un poco más abajo para los sets
-  tft.print("Sets: " + msgArray[0]);  // Mostrar los sets de Player 1
-
-  // Línea divisoria para los game sets de Player 1
-  tft.drawLine(10, 100, 310, 100, BLACK);  // Línea divisoria para Player 1
 
   // Mostrar game sets y puntos de Player 1 en la misma línea
-  tft.setCursor(40, 120);  // Un poco más abajo
-  tft.print("Games: " + msgArray[1]);  // Mostrar los games de Player 1
-  tft.setCursor(150, 120);  // Espacio para los puntos
-  tft.print("Points: " + msgArray[2]);  // Mostrar los puntos de Player 1
+  tft.setTextSize(3);
+  tft.setCursor(90, 110);  // Un poco más abajo
+  tft.print("Games: " + mensaje.msgArray[0]);  // Mostrar los games de Player 1
+  tft.setCursor(250, 110);  // Espacio para los puntos
+  tft.print("Points: " + mensaje.msgArray[1]);  // Mostrar los puntos de Player 1
 
-  // Línea divisoria entre Player 1 y Player 2
-  tft.drawLine(10, 140, 310, 140, BLACK);  // Línea divisoria entre Player 1 y Player 2
 
   // Configuración para Player 2
-  tft.setTextSize(3);  // Tamaño de texto más grande para Player 2
-  tft.setCursor(40, 160);  // Posicionar en la parte inferior del Player 1
+  tft.setTextSize(5);  // Tamaño de texto más grande para Player 2
+  tft.setCursor(125, 180);  // Posicionar en la parte inferior del Player 1
   tft.print("Player 2");
-  
-  tft.setTextSize(2);  // Tamaño de texto más pequeño para los sets
-  tft.setCursor(40, 200);  // Un poco más abajo para los sets
-  tft.print("Sets: " + msgArray[3]);  // Mostrar los sets de Player 2
-
-  // Línea divisoria para los game sets de Player 2
-  tft.drawLine(10, 220, 310, 220, BLACK);  // Línea divisoria para Player 2
 
   // Mostrar game sets y puntos de Player 2 en la misma línea
-  tft.setCursor(40, 240);  // Un poco más abajo
-  tft.print("Games: " + msgArray[4]);  // Mostrar los games de Player 2
-  tft.setCursor(150, 240);  // Espacio para los puntos
-  tft.print("Points: " + msgArray[5]);  // Mostrar los puntos de Player 2
+  tft.setTextSize(3);
+  tft.setCursor(90, 250);  // Un poco más abajo
+  tft.print("Games: " + mensaje.msgArray[2]);  // Mostrar los games de Player 2
+  tft.setCursor(250, 250);  // Espacio para los puntos
+  tft.print("Points: " + mensaje.msgArray[3]);  // Mostrar los puntos de Player 2
+}
+
+void tilesAvanzadas2players(Result mensaje) {
+  tft.fillScreen(BLACK);
+
+  // Configuración para Player 1 en la mitad superior de la pantalla
+  tft.setCursor(95, 40);  // Posicionar en la mitad superior de la pantalla
+  tft.setTextSize(6);
+  tft.print("Player 1");
+
+  tft.setCursor(120, 100);  // Un poco más abajo para el nombre del personaje
+  tft.setTextSize(4);
+  tft.print("Score: " + mensaje.msgArray[0]);
+
+  // Configuración para Player 2 en la mitad inferior de la pantalla
+  tft.setCursor(95, 190);  // Posicionar en la mitad inferior de la pantalla
+  tft.setTextSize(6);
+  tft.print("Player 2");
+
+  tft.setCursor(120, 250);  // Un poco más abajo para el nombre del personaje
+  tft.setTextSize(4);
+  tft.print("Score: " + mensaje.msgArray[1]);  // Mostrar el personaje que selecciona Player 2
 }
 
 void setup() {
@@ -131,15 +151,43 @@ void setup() {
   tft.begin(ID);
 
   tft.setRotation(1);  
-  tft.fillScreen(BLACK);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(2);  
+  tft.setTextColor(GREEN); 
+
+  prueba.msgArray[0]=200;
+  prueba.msgArray[1]=220;
+  tilesAvanzadas2players(prueba);
+
 }
 
 void loop() {
-  if (Serial.available()) {
+
+  /*
+  prueba.msgArray[0] = "3";
+  prueba.msgArray[1] = "30";
+  
+  prueba.msgArray[2] = "5";
+  prueba.msgArray[3] = "45";
+
+  mostrarPuntuacionesTennis(prueba);
+
+  delay(1000);
+
+  prueba.msgArray[0] = "peron";
+
+  mostrarSeleccion1Personajes(prueba);
+
+  
+  delay(1000);
+
+  prueba.msgArray[0] = "leo";
+  prueba.msgArray[1] = "fierro";
+
+  mostrarSeleccion2Personajes(prueba);
+
+  delay(1000);
+  */
+    if (Serial.available()) {
       String string2convert = Serial.readStringUntil('\n');
       Result mensaje = string2array(string2convert);
-
-  }
+    }
 }
