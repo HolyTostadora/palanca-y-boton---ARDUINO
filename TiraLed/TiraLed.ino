99d1a084e1909fdd24b5d45b52c09647096d764f
HolyTostadora <164009725+HolyTostadora@users.noreply.github.com>
2024-11-01 10:23:09 -0300

    ñ

--- TiraLed/TiraLed.ino
+++ TiraLed/TiraLed.ino
@@ -3,6 +3,8 @@
 #define NUM_LEDS 5
 #define LED_PIN 3
 
+#define DELAY 20
+
 CRGB leds [NUM_LEDS];
 
 void setup() {
@@ -10,15 +12,24 @@ void setup() {
   FastLED.setBrightness(50);
 
 }
+void blinkLeds(CRGB color) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = color;
+    FastLED.show();
+    delay(DELAY);
+
+    if (i + 1 < NUM_LEDS) {  // Check to avoid going out of bounds
+      leds[i + 1] = color;
+      FastLED.show();
+      delay(DELAY);
+    }
+
+    leds[i] = CRGB::Black;  // Turn off the current LED
+    FastLED.show();
+    delay(DELAY);
+  }
+}
 
 void loop() {
-  for (int i=0;i<NUM_LEDS;i++)
-  {
-    leds[i] = CRGB::Yellow;
-    FastLED.show();
-    delay(500);
-    leds[i] = CRGB::Black;
-    FastLED.show();
-    delay(500);
-  }
+  blinkLeds(CRGB::Blue);  // Call the function with any color you want, like CRGB::Blue
 }
