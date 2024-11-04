b133df4cebb59aa27800b744cb38961965f2211f
HolyTostadora <164009725+HolyTostadora@users.noreply.github.com>
2024-11-04 15:57:43 -0300

    leds

--- TiraLed/TiraLed.ino
+++ TiraLed/TiraLed.ino
@@ -1,35 +1,93 @@
 #include <FastLED.h>
 
 #define NUM_LEDS 5
-#define LED_PIN 3
+#define LED_PIN1 3
+#define LED_PIN2 4
 
-#define DELAY 20
-
-CRGB leds [NUM_LEDS];
+CRGB leds1 [NUM_LEDS];
+CRGB leds2 [NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<WS2812B,LED_PIN,GRB>(leds,NUM_LEDS);
+  
+  FastLED.addLeds<WS2812B,LED_PIN1,GRB>(leds1,NUM_LEDS);
+  FastLED.addLeds<WS2812B,LED_PIN2,GRB>(leds2,NUM_LEDS);
+  
   FastLED.setBrightness(50);
 
 }
-void blinkLeds(CRGB color) {
+
+void recorrer1(CRGB color, int DELAY) {
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = color;
+    leds1[i] = color;
     FastLED.show();
     delay(DELAY);
 
     if (i + 1 < NUM_LEDS) {  // Check to avoid going out of bounds
-      leds[i + 1] = color;
+      leds1[i + 1] = color;
       FastLED.show();
       delay(DELAY);
     }
 
-    leds[i] = CRGB::Black;  // Turn off the current LED
+    leds1[i] = CRGB::Black;  // Turn off the current LED
+    FastLED.show();
+    delay(DELAY);
+  }
+}
+
+void recorrer2(CRGB color, int DELAY) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds2[i] = color;
+    FastLED.show();
+    delay(DELAY);
+
+    if (i + 1 < NUM_LEDS) {  // Check to avoid going out of bounds
+      leds2[i + 1] = color;
+      FastLED.show();
+      delay(DELAY);
+    }
+
+    leds2[i] = CRGB::Black;  // Turn off the current LED
+    FastLED.show();
+    delay(DELAY);
+  }
+}
+
+void recorrerIdaYVuelta1(CRGB color, int DELAY) {
+  // Barrido hacia adelante
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds1[i] = color;
+    FastLED.show();
+    delay(DELAY);
+
+    if (i + 1 < NUM_LEDS) {
+      leds1[i + 1] = color;
+      FastLED.show();
+      delay(DELAY);
+    }
+
+    leds1[i] = CRGB::Black;  // Apaga el LED actual
+    FastLED.show();
+    delay(DELAY);
+  }
+
+  // Barrido hacia atrás
+  for (int i = NUM_LEDS - 1; i >= 0; i--) {
+    leds1[i] = color;
+    FastLED.show();
+    delay(DELAY);
+
+    if (i - 1 >= 0) {
+      leds1[i - 1] = color;
+      FastLED.show();
+      delay(DELAY);
+    }
+
+    leds1[i] = CRGB::Black;  // Apaga el LED actual
     FastLED.show();
     delay(DELAY);
   }
 }
 
 void loop() {
-  blinkLeds(CRGB::Blue);  // Call the function with any color you want, like CRGB::Blue
+  recorrerIdaYVuelta1(CRGB::Blue, 100);  // Llama a la función con el color y el retraso deseado
 }
